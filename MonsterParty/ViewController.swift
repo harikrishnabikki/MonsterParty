fa693c089038a991da030ef683735b42e1f60efc
Hari Krishna Bikki <bikkiharikrishna@gmail.com>
2018-08-05 10:19:32 +1000

    Initial Commit

---
+++ MonsterParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonsterParty
+//
+//  Created by Arshad Ameer Hamza on 5/8/18.
+//  Copyright © 2018 Hari Krishna Bikki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
